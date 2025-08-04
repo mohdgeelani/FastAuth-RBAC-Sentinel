22d19d774846fd8e8bbdfae46158c9e8aa41c099
Mohd Geelani Khan <mohdgeelanikhan25@gmail.com>
2025-08-05 03:07:33 +0530

    Add login, GET, and POST endpoints with JWT authentication

---
+++ app/auth.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import Session
+from app import models, database
+from app.database import get_db
+
+from fastapi import Depends, HTTPException, status
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+
+SECRET_KEY = "83e021ef33bb1a46a11b57fbb44aa9e3d9e3a35ffb2012bbcf54a3c94b7e44f2"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+print(OAuth2PasswordBearer)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp':expire})
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise credentials_exception
+        return user_id
+    except JWTError:
+        raise credentials_exception
+    
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception =  HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    user_id = verify_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    return user
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app import models, schemas, database
+from app.models import User
+from app.schemas import UserOut
+from app.database import get_db
+from app.auth import get_current_user 
 from passlib.context  import CryptContext
 
 router = APIRouter()
@@ -11,12 +15,12 @@ pwd_context = CryptContext(schemes= ["bcrypt"], deprecated = "auto")
 # This is a dependency function that provides a SQLAlchemy session to any route that includes db: Session = Depends(get_db)
 
 
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db():
+#     db = database.SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 @router.post("/register", response_model = schemas.UserOut)
 
@@ -24,8 +28,8 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     existing_user = db.query(models.User).filter(models.User.email == user.email).first()
     if existing_user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered"
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Email already registered"
         )
     hashed_password =  pwd_context.hash(user.password)
     new_user = models.User(
@@ -38,3 +42,7 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.get("/users/me", response_model=UserOut)
+def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
